41b822806b9f610ffc27271d191f51199abbf94f
Gladwell-Wanjiku <wanjeriwanjikugladwell@gmail.com>
2024-03-29 04:46:44 +0300

    add arrays

---
+++ function.js
@@ -0,0 +1,24 @@
+let array1 = ["dresses", "trousers", "sweater", "blouse"]
+function array1sort(array1){
+    array1.sort();
+    array1.reverse();
+    return array1;
+}
+console.log(array1sort(array1))
+
+var a = [20,30,40,-16,-12,-10]
+function array(){
+let positive= a.filter((element)=>
+element<0);
+let zero = a.filter ((element) =>
+element==0)
+return{
+    positive:positive
+}
+}
+
+//4
+let arrayNum = [20,15,34,76]
+function arrayNum.array.forEach(element => {
+    console.log(elements*2)
+});
